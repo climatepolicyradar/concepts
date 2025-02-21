b2a9ab0342441d296bd3520983002fd679b1cf0d
James Gorrie <james.gorrie@climatepolicyradar.org>
2025-02-21 13:44:57 +0000

    first commit

--- scripts/check_aws_credentials.py
+++ scripts/check_aws_credentials.py
@@ -3,6 +3,6 @@ import boto3
 session = boto3.Session()
 creds = session.get_credentials()
 if creds is None:
-    print("No credentials found.")
+    raise Exception("No credentials found.")
 else:
     print("Credentials loaded:", creds)
