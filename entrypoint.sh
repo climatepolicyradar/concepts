4f5f3873197e4d97603b332a54a4152d1e92cc06
James Gorrie <james.gorrie@climatepolicyradar.org>
2025-02-21 15:38:42 +0000

    first commit

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,4 +4,4 @@
 mkdir s3-concepts
 aws s3 sync s3://cpr-production-document-cache/concepts ./s3-concepts
 python3 ./create_duckdb.py
-fastapi run server.py --port 8080
+fastapi run ./app/api.py --port 8080
