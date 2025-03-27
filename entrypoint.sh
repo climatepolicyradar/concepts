d9148362e6331b67ca2989e1d1b409e7c3091b3a
Katy Baulch <46493669+katybaulch@users.noreply.github.com>
2025-03-27 11:00:00 +0000

    Add error handling to entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # TODO: This is probably not the place to do this as it increases startup time
 # significantly, but I can't seem to work out where else we can do it with the right permissions
-mkdir s3-concepts
+mkdir -p s3-concepts
 aws s3 sync s3://cpr-production-document-cache/concepts ./s3-concepts
 python3 ./create_duckdb.py
 fastapi run ./app/api.py --port 8080
